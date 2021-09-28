cb22178b276c4a8a31070589d611fdf5d28a328d
Shawn W.S. Wong <wangyu_hfut@163.com>
2021-09-28 15:19:12 +0800

    init docker composer config file

---
+++ README.md
@@ -0,0 +1,8 @@
+# 创建网络
+`
+sh network.sh
+`
+# 安装docker
+`
+docker-compose up -d
+`
---
+++ network.sh
@@ -0,0 +1,1 @@
+docker network create --driver bridge --subnet 172.75.1.0/25 --gateway 172.75.1.1  letsparty_network
